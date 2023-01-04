606c8c31f5c33b018e202c890303c1bd84b1a0be
Siddharth Venigalla <sidthokala21@gmail.com>
2023-01-03 21:44:32 -0500

    Sid is the GOAT

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, CodeCrew!")
+            Text("Sid is the GOAT")
         }
         .padding()
     }
