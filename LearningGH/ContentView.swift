8b80199a5d7f6a490e572bb760924b6f63afe1c9
sidthekid721 <121835397+sidthekid721@users.noreply.github.com>
2023-01-04 01:10:52 -0500

    Change to Whole Lotta Red

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(Color.pink)
-            Text("Lil Uzi Pink Tape")
+            Text("Playboi Carti Whole Lotta Red")
         }
         .padding()
     }
